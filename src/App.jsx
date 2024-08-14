7c7655d318fd01be041fa8fd3a42eafae9098ccf
Shayaan Ahmed Farooqi <fleshlime.1@gmail.com>
2024-08-14 11:08:00 +0500

    setup antd theme and add react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Layout } from "antd";
+const { Header, Content, Sider, Footer } = Layout;
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Layout style={{ minHeight: "100vh" }}>
+        <Header></Header>
+        <Layout>
+          <Sider width={200}></Sider>
+          <Content
+            style={{
+              padding: 24,
+              margin: 0,
+              background: "lightgray",
+            }}
+          ></Content>
+        </Layout>
+        <Footer style={{ background: "yellow" }}></Footer>
+      </Layout>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { ConfigProvider, theme } from "antd";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/index.js";
 
-createRoot(document.getElementById('root')).render(
+// refer to https://ant.design/docs/react/customize-theme#seedtoken for docs on antd design tokens for theme customization
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ConfigProvider
+      theme={{
+        cssVar: true,
+        hashed: false,
+        algorithm: theme.defaultAlgorithm,
+        token: {
+          colorPrimary: "#602eff",
+        },
+      }}
+    >
+      <RouterProvider router={router} />
+    </ConfigProvider>
+  </StrictMode>
+);
