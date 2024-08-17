da2a1bd0619c6b1df02adbbe1fa9033eea146bc3
Shayaan Ahmed Farooqi <fleshlime.1@gmail.com>
2024-08-17 09:46:47 +0500

    add react query

--- src/main.jsx
+++ src/main.jsx
@@ -3,21 +3,36 @@ import { createRoot } from "react-dom/client";
 import { ConfigProvider, theme } from "antd";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/index.js";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      retry: 3,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 // refer to https://ant.design/docs/react/customize-theme#seedtoken for docs on antd design tokens for theme customization
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ConfigProvider
-      theme={{
-        cssVar: true,
-        hashed: false,
-        algorithm: theme.defaultAlgorithm,
-        token: {
-          colorPrimary: "#602eff",
-        },
-      }}
-    >
-      <RouterProvider router={router} />
-    </ConfigProvider>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <ConfigProvider
+        theme={{
+          cssVar: true,
+          hashed: false,
+          algorithm: theme.defaultAlgorithm,
+          token: {
+            colorPrimary: "#602eff",
+          },
+        }}
+      >
+        <RouterProvider router={router} />
+      </ConfigProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
