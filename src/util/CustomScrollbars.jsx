2b5db960c869e56413ca8fedd4e14a281be1920f
Shayaan Ahmed Farooqi <fleshlime.1@gmail.com>
2024-08-18 14:04:41 +0500

    copy over util

---
+++ src/util/CustomScrollbars.jsx
@@ -0,0 +1,15 @@
+const CustomScrollbars = () =>
+  null;
+  //   <Scrollbars
+  //     {...props}
+  //     autoHide
+  //     renderTrackHorizontal={props => (
+  //       <div
+  //         {...props}
+  //         style={{ display: "none" }}
+  //         className="track-horizontal"
+  //       />
+  //     )}
+  //   />
+
+export default CustomScrollbars;
