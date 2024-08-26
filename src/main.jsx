9212f0afb1b8126b2a769b1d82679e01a8e307b2
Shayaan Ahmed Farooqi <fleshlime.1@gmail.com>
2024-08-26 23:00:27 +0500

    reorg import

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,8 @@ import { ConfigProvider, theme } from "antd";
 import { RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import "antd/dist/reset.css";
 import { router } from "./routes";
+import "antd/dist/reset.css";
 
 const queryClient = new QueryClient({
   defaultOptions: {
