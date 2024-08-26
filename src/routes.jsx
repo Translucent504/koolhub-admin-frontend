9bb82482c97b597ee0bd9ac2c5c83e3f047facaf
Shayaan Ahmed Farooqi <fleshlime.1@gmail.com>
2024-08-26 19:47:13 +0500

    add error element to root route

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,10 +2,12 @@ import { createBrowserRouter } from "react-router-dom";
 import RootLayout from "./components/RootLayout";
 import { HomePage } from "features/dashboard";
 import { SamplePage } from "features/sample";
+import ErrorPage from "components/ErrorPage";
 
 export const router = createBrowserRouter([
   {
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: "/",
