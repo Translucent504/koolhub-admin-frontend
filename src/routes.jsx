1464c9520d587ff3597d18ef2f4ace7de9c47106
Shayaan Ahmed Farooqi <fleshlime.1@gmail.com>
2024-08-28 20:35:39 +0500

    update route structure with Anonymous and Authenticated routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,32 +1,60 @@
-import { createBrowserRouter } from "react-router-dom";
-import RootLayout from "./components/RootLayout";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 import { HomePage } from "features/dashboard";
 import { SamplePage } from "features/sample";
 import ErrorPage from "components/ErrorPage";
 import SampleDetailPage from "features/sample/SampleDetailPage";
 import SampleCreatePage from "features/sample/SampleCreatePage";
+import {
+  AuthLayout,
+  RequireAnonymous,
+  RequireAuth,
+  SignIn,
+} from "features/auth/components";
+
+function DefaultHomeRoute() {
+  return <Navigate to="/dashboard" />;
+}
 
 export const router = createBrowserRouter([
   {
-    element: <RootLayout />,
+    element: <RequireAuth />,
     errorElement: <ErrorPage />,
     children: [
       {
         path: "/",
-        element: <HomePage />,
-        index: true,
-      },
-      {
-        path: "sample",
         children: [
-          { index: true, element: <SamplePage /> },
+          { index: true, element: <DefaultHomeRoute /> },
           {
-            path: "new",
-            element: <SampleCreatePage />,
+            path: "dashboard",
+            element: <HomePage />,
           },
           {
-            path: ":sampleId",
-            element: <SampleDetailPage />,
+            path: "sample",
+            children: [
+              { index: true, element: <SamplePage /> },
+              {
+                path: "new",
+                element: <SampleCreatePage />,
+              },
+              {
+                path: ":sampleId",
+                element: <SampleDetailPage />,
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    element: <RequireAnonymous />,
+    children: [
+      {
+        element: <AuthLayout />,
+        children: [
+          {
+            path: "/sign-in",
+            element: <SignIn />,
           },
         ],
       },
