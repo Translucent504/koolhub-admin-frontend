eb714b4a4341b0351808f386314d6a09b50bf8c7
Shayaan Ahmed Farooqi <fleshlime.1@gmail.com>
2024-08-29 22:37:47 +0500

    update homepage

--- src/features/dashboard/HomePage.jsx
+++ src/features/dashboard/HomePage.jsx
@@ -4,25 +4,7 @@ import Glance from "./Glance";
 const Component = () => {
   return (
     <Row gutter={[24, 24]}>
-      <Col xl={8} lg={8} md={24} sm={24} xs={24}>
-        <Glance />
-      </Col>
-      <Col xl={8} lg={8} md={24} sm={24} xs={24}>
-        <Glance />
-      </Col>
-      <Col xl={8} lg={8} md={24} sm={24} xs={24}>
-        <Glance />
-      </Col>
-      <Col xl={8} lg={8} md={24} sm={24} xs={24}>
-        <Glance />
-      </Col>
-      <Col xl={8} lg={8} md={24} sm={24} xs={24}>
-        <Glance />
-      </Col>
-      <Col xl={8} lg={8} md={24} sm={24} xs={24}>
-        <Glance />
-      </Col>
-      <Col xl={8} lg={8} md={24} sm={24} xs={24}>
+      <Col>
         <Glance />
       </Col>
     </Row>
