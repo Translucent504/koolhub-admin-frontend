044349f873f258e3750ba0305d4ba560a8af1e00
Shayaan Ahmed Farooqi <fleshlime.1@gmail.com>
2024-09-07 13:27:03 +0500

    update auth service and axios interceptors

--- src/@mb/api/axios.js
+++ src/@mb/api/axios.js
@@ -10,15 +10,12 @@ let api = axios.create({
 api.interceptors.response.use(
   (response) => response,
   (error) => {
-    // whatever you want to do with the error
-    //debugger;
     switch (error.response.status) {
       case 401:
-        clearAuthentication();
+        clearAuthentication(api);
         break;
       default:
         console.log(error.response);
-      //window.location = "/500"
     }
     return Promise.reject(error);
   }
--- src/features/auth/service.js
+++ src/features/auth/service.js
@@ -4,14 +4,18 @@ export const getUserDetails = async () => {
   return storage.getObject("user");
 };
 
-export const clearAuthentication = async () => {
+export const clearAuthentication = async (api) => {
   // update axios token header
   storage.clear();
+  delete api.defaults.headers["Authorization"];
 };
 
-export const authenticateUser = async () => {
+export const authenticateUser = async (api) => {
+  const token = "";
   // update axios token header
   const user = { name: "Admin" };
   storage.setObject("user", user);
+  api.defaults.headers["Authorization"] = `Bearer ${token}`;
+
   return user;
 };
