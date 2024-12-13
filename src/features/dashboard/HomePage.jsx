d1b61abdf7465b933d695420f9cb8e8d3a414e5f
Syed Ashhad <syedashad17@gmail.com>
2024-12-13 11:55:16 +0500

    `Removed RoomOccupancy component from HomePage component`

--- src/features/dashboard/HomePage.jsx
+++ src/features/dashboard/HomePage.jsx
@@ -3,7 +3,6 @@ import Announcements from "./Announcements/Announcements";
 import EmployeeCounter from "./EmployeeCounter/EmployeeCounter";
 import ExitStudentTracking from "./ExitStudentTracking/ExitStudentTracking";
 import Glance from "./Glance/Glance";
-import RoomOccupancy from "./RoomOccupancy/RoomOccupancy";
 
 const Component = () => {
   return (
@@ -14,9 +13,6 @@ const Component = () => {
         </Col>
         <Col xs={24} sm={24} md={12} lg={8} xl={8}>
           <Announcements />
-        </Col>
-        <Col xs={24} sm={24} md={12} lg={8} xl={8}>
-          <RoomOccupancy/>
         </Col>
       </Row>
 
